95085772e5ec310238c460f1077bd56a2ce2f16a
Eman-Sm <135334795+Eman-Sm@users.noreply.github.com>
2024-01-05 20:11:24 +0700

    Add files via upload

---
+++ undangan.js
@@ -0,0 +1,125 @@
+simplyCountdown(".simply-countdown", {
+  year: 2024, // required
+  month: 2, // required
+  day: 3, // required
+  hours: 8, // Default is 0 [0-23] integer
+  minutes: 0, // Default is 0 [0-59] integer
+  seconds: 0, // Default is 0 [0-59] integer
+  words: {
+    //words displayed into the countdown
+    days: { singular: "hari", plural: "hari" },
+    hours: { singular: "jam", plural: "jam" },
+    minutes: { singular: "meenit", plural: "menit" },
+    seconds: { singular: "detik", plural: "detik" },
+  },
+  plural: true, //use plurals
+  inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
+  inlineClass: "simply-countdown-inline", //inline css span class in case of inline = true
+  // in case of inline set to false
+  enableUtc: false,
+  onEnd: function () {
+    // your code
+    return;
+  },
+  refresh: 1000, //default refresh every 1s
+  sectionClass: "simply-section", //section css class
+  amountClass: "simply-amount", // amount css class
+  wordClass: "simply-word", // word css class
+  zeroPad: false,
+  countUp: false, // enable count up if set to true
+});
+
+// hamburger klik
+
+const stickyTop = document.querySelector(".sticky-top");
+const offcanvas = document.querySelector(".offcanvas");
+
+offcanvas.addEventListener("show.bs.offcanvas", function () {
+  stickyTop.style.overflow = "visible";
+});
+
+offcanvas.addEventListener("hidden.bs.offcanvas", function () {
+  stickyTop.style.overflow = "hidden";
+});
+
+// disabled scroll
+
+const rootElement = document.querySelector(":root");
+const audioIconWrapper = document.querySelector(".audio-icon-wrapper");
+const audioIcon = document.querySelector(".audio-icon-wrapper i");
+const song = document.querySelector("#song");
+let isPlaying = false;
+
+function disableScroll() {
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+  window.onscroll = function () {
+    window.scrollTo(scrollTop, scrollLeft);
+  };
+
+  rootElement.style.scrollBehavior = "auto";
+}
+
+function enableScroll() {
+  window.onscroll = function () {};
+  rootElement.style.scrollBehavior = "smooth";
+  // localStorage.setItem("opened", "true");
+  playAudio();
+}
+function playAudio() {
+  song.volume = 0.1;
+  audioIconWrapper.style.display = "flex";
+  song.play();
+  isPlaying = true;
+}
+
+audioIconWrapper.onclick = function () {
+  if (isPlaying) {
+    song.pause();
+    audioIcon.classList.remove("bi-disc");
+    audioIcon.classList.add("bi-pause-circle");
+  } else {
+    song.play();
+    audioIcon.classList.add("bi-disc");
+    audioIcon.classList.remove("bi-pause-circle");
+  }
+
+  isPlaying = !isPlaying;
+};
+
+// if (!localStorage.getItem("opened")) {
+// disableScroll();
+// }
+
+disableScroll();
+
+// form to drive
+
+window.addEventListener("load", function () {
+  const form = document.getElementById("my-form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: "POST",
+      body: data,
+    }).then(() => {
+      alert("konfirmasi kehadiran berhasil terkirim!");
+    });
+  });
+});
+
+// nama undangan
+
+const urlParams = new URLSearchParams(window.location.search);
+const nama = urlParams.get("n") || "";
+const pronoun = urlParams.get("p") || "Bapak/Ibu/Saudara/i";
+
+const namaContainer = document.querySelector(".hero h4 span");
+namaContainer.innerText = `${pronoun} ${nama},`.replace(/ ,$/, ",");
+
+document.querySelector("#nama").value = nama;
+
+// ?p=...&N=...
